540109a89a0c225e6e4d6b02f11c7170b33875cb
jagriti2004 <prakashjagriti32@gmail.com>
2025-06-11 15:44:09 +0530

    ABC

--- resources/views/base/header.blade.php
+++ resources/views/base/header.blade.php
@@ -69,7 +69,7 @@
 
   <nav class="navbar navbar-expand-lg bg-white shadow-sm">
     <div class="container-fluid px-4">
-      <a class="navbar-brand logo" href="#">NYKA</a>
+      <a class="navbar-brand logo" href="#">NYKAA</a>
       <div class="collapse navbar-collapse">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item"><a class="nav-link" href="#">Categories</a></li>
