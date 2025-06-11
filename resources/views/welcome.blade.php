2510e13eca2a67a4855e21d13d57bdbb1f0210e3
jagriti2004 <prakashjagriti32@gmail.com>
2025-06-11 15:50:48 +0530

    SDFS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,3 +4,5 @@
 <h2>Welcome to the Laravel Application</h2>
 
 <p>This is a simple Laravel application to demonstrate the basic setup.</p>
+<p>Feel free to explore the code and make changes as needed.</p>
+<p>Enjoy coding!</p>
