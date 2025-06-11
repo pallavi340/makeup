7dcdb73f1045c239c5b4f8d2f5acac6c9546de5b
jagriti2004 <prakashjagriti32@gmail.com>
2025-06-11 15:31:05 +0530

    2commit

---
+++ resources/views/base/home.blade.php
@@ -0,0 +1,17 @@
+@extends('base.header')
+@section('content')
+<div class="container my-5">
+    <h2 class="promo-title">✨ Mid-Year Beauty Lineup ✨</h2>
+    <div class="row g-4">
+      <div class="col-md-4">
+        <div class="card promo-card h-100">
+          <img src="https://media6.ppl-media.com/tr:h-750,w-750,c-at_max,dpr-2/static/img/product/343127/dot-and-key-pomegranate-miracle-vitamin-e-revitalizing-moisturizer-spf-30-60-ml_8_display_1682340993_7db6e50a.jpgg" class="card-img-top promo-img" alt="Dot & Key Offer">
+        </div>
+      </div>
+      
+
+    </div>
+  </div>
+  @endsection
+
+  
